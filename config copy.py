c5e2e1a134f6ee8f23dc4a760e6c2a24af4c46da
ansiabdo <ansi.abdooh@gmail.com>
2023-01-28 00:21:48 +0300

    Update and rename config copy.py to example.config.py

--- config copy.py
+++
@@ -1,4 +0,0 @@
-API_KEY="your api key"
-
-# from this url https://beta.openai.com/account/api-keys 
-# press on Create New Security Key
---
+++ example.config.py
@@ -0,0 +1,5 @@
+API_KEY="your api key"
+
+#1- rename example.config.py to config.py
+#2- from this url https://beta.openai.com/account/api-keys
+#3- press on Create New Security Key and copy the new generated key and add it to config.py
